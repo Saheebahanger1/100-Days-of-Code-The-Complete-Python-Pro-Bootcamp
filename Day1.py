a65998fd7f65e4ab591f006bd2df67fd5c67c32a
Saheeb Ahanger <saheebahanger1@gmail.com>
2026-01-16 14:26:45 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,64 +1,89 @@
-# Printing to the console
+"""
+Python Day 1 Basics
+Author: Mohammed Saheeb
+Description:
+This program demonstrates basic Python concepts such as:
+- Printing to the console
+- New line printing
+- String concatenation
+- Taking user input
+- Variables
+- Calculating string length
+- A simple Band Name Generator project
+"""
 
+
+# Printing a simple message to the console
 print("Hello Saheeb!")
-#output= Hello Saheeb!
+
+# Output:
+# Hello Saheeb!
 
 
-# Printing sentences in the next line 
+# \n is used to move text to the next line
+print("Hello Saheeb! \nHello World")
 
-print("Hello Saheeb! \n Hello World ")
-#output= Hello Saheeeb!
-#Hello World
+# Output:
+# Hello Saheeb!
+# Hello World
 
 
-# Concatenaion of two or more strings
-
+# Joining two strings using the + operator
 print("Hello " + "Saheeb")
-#output= Hello Saheeb
+
+# Output:
+# Hello Saheeb
 
 
-#Taking input from user
+# Asking the user to enter their name
+input("What is your Name? ")
 
-input("What is your Name?")
-#output= what is your Name? _ Saheeb
+# Output (example):
+# What is your Name? Saheeb
 
 
-#Making variables
-
+# Storing a value in a variable
 Name = "Saheeb"
+
+# Printing the value stored in the variable
 print(Name)
-#output= Saheeb
+
+# Output:
+# Saheeb
 
 
-#Calculating Length of string
+# Assigning a string to a variable
+Name = "Saheeb"
 
-Name = 'Saheeb'
+# Calculating and printing the length of the string
 print(len(Name))
-#output= 6
+
+# Output:
+# 6
 
 
-#Taking input from user & Calculating its length
-
+# Taking name input from the user
 username = input("What is your name? ")
+
+# Calculating the length of the input
 length = len(username)
+
+# Printing the length
 print(length)
-#output= What is your name? _Saheeb
-#6
+
+# Output (example):
+# What is your name? Saheeb
+# 6
 
 
-#Day 1 Project - BAND NAME GENERATOR
-
+# Displaying welcome message
 print("Welcome to the Band Name Generator.")
+
+# Asking the user for city name
 city = input("Which city did you grow up in?\n")
+
+# Asking the user for pet name
 pet = input("What is the name of a pet?\n")
+
+# Generating and displaying the band name
 print("Your band name could be: " + city + " " + pet)
-#output= Welcome to the Band Name Generator.
-#Which city did you grow up in?
-#_Baramulla
-#What is the name of a pet?
-#_Tiger
-#Your band name could be: Baramulla Tiger.
-
-
-
-#Day 1 is completed!
