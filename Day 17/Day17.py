400f4f294d24a94ee8876f129730d2f45768e7a2
Saheeb Ahanger <saheebahanger1@gmail.com>
2026-02-12 14:04:19 +0530

    Add files via upload

---
+++ Day 17/Day17.py
@@ -0,0 +1,42 @@
+# Classes and Objects.
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "Saheeb")
+user_2 = User("002", "Khalid")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+
+
+# Quiz Game Project.
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{len(question_bank)}")
+
